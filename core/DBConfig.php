26bbff47b14fa4311c46d7159a83e1d3cab526a3
Andy S Kirk <andys@perception-sas.com>
2020-06-28 21:59:12 +0100

    release to fix a couple bugs

--- core/DBConfig.php
+++ core/DBConfig.php
@@ -5,6 +5,6 @@ namespace DBConfig;
 class DBConfig {
     public $server = "localhost";
     public $db_name = "postcode";
-    public $db_user = "sys_user";
-    public $db_pass = "fdsfsdfsdfkJFKADJSOI121!!";
+    public $db_user = "user";
+    public $db_pass = "dbpass";
 }
